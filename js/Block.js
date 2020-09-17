544b55747ffc746251c420e78ed2c664ad78e92c
mrinal17-code <66773332+mrinal17-code@users.noreply.github.com>
2020-09-17 17:47:56 +0530

    Add files via upload

---
+++ js/Block.js
@@ -0,0 +1,50 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :0.0,
+            //isStatic:true
+           
+        }
+        this.visibility =255;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        
+        
+
+      }
+
+      display(){
+      
+      if(this.body.speed<5){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+      else{
+        World.remove(world,this.body);
+         push();
+        this.Visibility = this.Visibility-5;
+
+        tint(255,this.Visibility);
+        fill(this.Visibility);
+       
+        pop();
+
+      }
+      }
+      
+     score(){
+        if (this.Visibility<0 && this.Visibility>=1005){
+          score++;
+        }
+      }
+}
